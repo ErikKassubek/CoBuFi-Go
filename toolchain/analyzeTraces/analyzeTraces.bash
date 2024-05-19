2f35324d4dbb7cba3f63c1803617567136f2ccc1
yellow-tshirt <mar.occhinegro@gmail.com>
2024-05-19 14:33:52 +0200

    added progress

--- toolchain/analyzeTraces/analyzeTraces.bash
+++ toolchain/analyzeTraces/analyzeTraces.bash
@@ -1,15 +1,15 @@
 #!/bin/bash
-# Check if directory path is provided
 if [ -z "$1" ]; then
     echo "Usage: $0 <directory>"
     exit 1
 fi
 dir_path=$1
 directories=$(find "$dir_path" -type d -name "advocateTrace")
+num_dirs=$(echo "$directories" | wc -l)
+current_dir=1
 for dir in $directories; do
-    echo "Directory: $dir"
-    for file in "$dir"/*; do
-        echo "File: $file"
-        cat "$file"
-    done
+    # print progress
+    echo "Processing directory $current_dir of $num_dirs"
+    echo "Processing directory: $dir"
+    current_dir=$((current_dir+1))
 done
