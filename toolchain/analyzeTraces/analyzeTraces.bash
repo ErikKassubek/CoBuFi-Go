3122e8895328e1f3675bbfcd91acf65b9b38ae86
yellow-tshirt <mar.occhinegro@gmail.com>
2024-05-20 09:27:27 +0200

    finish script that analyze all advocateResult directories and save in advocateAnalysis in adjacent directory

--- toolchain/analyzeTraces/analyzeTraces.bash
+++ toolchain/analyzeTraces/analyzeTraces.bash
@@ -15,8 +15,6 @@ current_dir=1
 for dir in $directories; do
     echo "Processing directory $current_dir of $num_dirs"
     echo "Directory Name: $dir"
-    echo "$path_to_analyzer -t $dir"
-    $path_to_analyzer -t $dir
-    
+    $path_to_analyzer -t $dir &>"$dir/../advocateAnalysis.txt"
     current_dir=$((current_dir+1))
 done
